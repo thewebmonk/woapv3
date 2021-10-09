b6ebca282a084422a3f809d6805c878848436516
Ashish Patel <aspatel808@gmail.com>
2021-10-10 00:08:51 +0530

    First Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,34 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "Writing Habit",
+    title: "Writing Habit"
   },
   plugins: [
     "gatsby-plugin-sass",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
-  ],
+    {
+      resolve: "gatsby-plugin-prettier-eslint",
+      options: {
+        prettier: {
+          patterns: [
+            // the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
+            "**/*.{css,scss,less}",
+            "**/*.{json,json5}",
+            "**/*.{graphql}",
+            "**/*.{md,mdx}",
+            "**/*.{html}",
+            "**/*.{yaml,yml}"
+          ]
+        },
+        eslint: {
+          patterns: "**/*.{js,jsx,ts,tsx}",
+          customOptions: {
+            fix: true,
+            cache: true
+          }
+        }
+      }
+    }
+  ]
 };
