90de2517d3f63cefb457c815ebe79430bf22be62
Ashish Patel <ashishpatel@Ashishs-MacBook-Air.local>
2022-01-02 01:29:50 +0530

    added gatsby ssr config

---
+++ gatsby-ssr.js
@@ -0,0 +1,10 @@
+exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  const headComponents = getHeadComponents();
+  const styleHeadComponents = headComponents.filter(
+    (component) => component.type === 'style'
+  );
+  const nonStyleHeadComponents = headComponents.filter(
+    (component) => component.type !== 'style'
+  );
+  replaceHeadComponents([nonStyleHeadComponents, styleHeadComponents]);
+};
