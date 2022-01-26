8018b8a2a9e06e8144caa02d0f5a44277f810945
Ashish Patel <ashishpatel@Ashishs-MacBook-Air.local>
2022-01-27 00:03:05 +0530

    fixed import

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -3,7 +3,7 @@ import NavBar from '../components/nav';
 import Footer from '../components/Footer';
 import '../assets/scss/pages.scss';
 import '../assets/scss/resume.scss';
-import Breadcrumb from '../components/common/Broadcrumb/breadcrumb';
+import Breadcrumb from '../components/common/Broadcrumb/Breadcrumb';
 import { object } from 'prop-types';
 import { resume } from '../data/resume-data';
 import SEO from '../components/common/Seo';
