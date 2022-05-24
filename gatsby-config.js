ae560a3abf0907fea60e3df5be83f1f61db61b6a
Ashish Patel <aspatel808@gmail.com>
2022-05-24 23:00:34 +0530

    GA 4 upgrade

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,9 +45,15 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: process.env.GA_TRACKING_ID
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: ['UA-142575352-1', 'G-00XPM1VYNE'],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        pluginConfig: {
+          head: true
+        }
       }
     }
   ]
